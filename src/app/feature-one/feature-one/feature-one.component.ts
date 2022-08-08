b9e8591a025d73f9f17bc192feb87e38df28bc5a
kar-21 <karthiks210198@gmail.com>
2022-08-08 19:23:09 +0530

    adding all the life cycle hooks in the component

--- src/app/feature-one/feature-one/feature-one.component.ts
+++ src/app/feature-one/feature-one/feature-one.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { LoggerService } from 'src/app/core/service/logger.service';
 
 @Component({
   selector: 'app-feature-one',
   templateUrl: './feature-one.component.html',
-  styleUrls: ['./feature-one.component.scss']
+  styleUrls: ['./feature-one.component.scss'],
 })
-export class FeatureOneComponent implements OnInit {
+export class FeatureOneComponent
+  implements
+    OnChanges,
+    OnInit,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
+  constructor(private loggerService: LoggerService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnChanges(changes: SimpleChanges): void {
+    this.loggerService.log('ngOnChanges');
   }
-
+  ngOnInit(): void {
+    this.loggerService.log('ngOnInit');
+  }
+  ngDoCheck(): void {
+    this.loggerService.log('ngDoCheck');
+  }
+  ngAfterContentInit(): void {
+    this.loggerService.log('ngAfterContentInit');
+  }
+  ngAfterContentChecked(): void {
+    this.loggerService.log('ngAfterContentChecked');
+  }
+  ngAfterViewInit(): void {
+    this.loggerService.log('ngAfterViewInit');
+  }
+  ngAfterViewChecked(): void {
+    this.loggerService.log('ngAfterViewChecked');
+  }
+  ngOnDestroy(): void {
+    this.loggerService.log('ngOnDestroy');
+  }
 }
